ba1d3468ce0d84c8f872710da4738fb0d059b107
Mark Waterous <markwaterous@gmail.com>
2025-09-23 18:24:17 -0600

    feat: add create user, post, publish post, delete

--- src/index.ts
+++ src/index.ts
@@ -26,8 +26,51 @@ app.get('/users', async (_, res) => {
   })
 
   // Only return users with more than 2 posts
-  const filteredUsers = users.filter(user => user.posts.length > 2)
+  const filteredUsers = users.filter((user) => user.posts.length > 2)
   res.json(filteredUsers)
+})
+
+app.post('/user', async (req, res) => {
+  const { name, email, bio } = req.body
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email,
+      profile: { create: { bio } },
+    },
+    include: { profile: true },
+  })
+  res.json(user)
+})
+
+app.post('/post', async (req, res) => {
+  const { title, content, authorEmail } = req.body
+  const post = await prisma.post.create({
+    data: {
+      title,
+      content,
+      author: { connect: { email: authorEmail } },
+    },
+    include: { author: true },
+  })
+  res.json(post)
+})
+
+app.put('/post/:id/publish', async (req, res) => {
+  const { id } = req.params
+  const post = await prisma.post.update({
+    where: { id: Number(id) },
+    data: { published: true },
+  })
+  res.json(post)
+})
+
+app.delete('/post/:id', async (req, res) => {
+  const { id } = req.params
+  const post = await prisma.post.delete({
+    where: { id: Number(id) },
+  })
+  res.json(post)
 })
 
 app.listen(port, () => {
