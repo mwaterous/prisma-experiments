8139681d06d305ac98492d3ab0d807ff7b5622c4
Mark Waterous <markwaterous@gmail.com>
2025-09-23 18:19:56 -0600

    feat: user endpoints

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,27 @@ app.use(express.json())
 
 const prisma = new PrismaClient()
 
+app.get('/user/:id', async (req, res) => {
+  const { id } = req.params
+  const user = await prisma.user.findUnique({
+    where: { id: Number(id) },
+    include: { profile: true, posts: true },
+  })
+  res.json(user)
+})
+
 app.get('/users', async (_, res) => {
-  const users = await prisma.user.findMany()
-  res.json(users)
+  const users = await prisma.user.findMany({
+    where: {
+      email: { endsWith: '@outlook.com' },
+      posts: { some: {} },
+    },
+    include: { profile: true, posts: true },
+  })
+
+  // Only return users with more than 2 posts
+  const filteredUsers = users.filter(user => user.posts.length > 2)
+  res.json(filteredUsers)
 })
 
 app.listen(port, () => {
